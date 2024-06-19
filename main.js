1e83c430c257daac2fe319a83149fc5eb4924d58
Benc8 <menyhert.bence08@gmail.com>
2024-06-19 15:32:25 +0200

    almost functioning

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-
+const addGUI = document.getElementById("addGUI");
+const cardHolder = document.getElementById("cardHolder");
+const mainText = ["KYS2","KYS4"];
+const mainTextHolder = document.getElementById("mainTextHolder");
 
 function addElementGUI(){
-
+    addGUI.style.display = "block";
 };
+
+function closeGUI(){
+    addGUI.style.display = "none";
+    reloadWindow();
+}
+
+function reloadWindow(){
+    mainText.forEach(element => {
+        let div = document.createElement("div");
+        let heading = document.createElement("h3");
+        let text = document.createTextNode(element);
+
+        heading.appendChild(text);
+        div.appendChild(heading);
+        div.classList.add("toDoItem");
+        cardHolder.appendChild(div);
+    });
+}
+
+function addElement(){
+    mainText += mainTextHolder.nodeValue;
+}
